fbb6ff0e2a7b833da5febdb9d0ea698f2b4b3df8
Max <max@teamatb.com>
2015-09-27 14:49:14 -0400

    Add standard module and test npm script for it, conform style

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-import p5 from 'p5'
+import P5 from 'p5'
 import 'p5/lib/addons/p5.sound'
 import 'p5/lib/addons/p5.dom'
 
-const s = function( p ) {
-  let x = 100;
-  let y = 100;
+const s = function (p) {
+  let x = 100
+  let y = 100
 
-  p.setup = function() {
+  p.setup = function () {
+    p.createCanvas(900, 900)
+  }
 
-    p.createCanvas(900, 900);
-  };
-
-  p.draw = function() {
+  p.draw = function () {
     p.background(0)
     p.fill(255)
-    p.rect(x,y,50,50)
+    p.rect(x, y, 50, 50)
   }
 }
 
-const sketch = new p5(s)
+new P5(s)
 
